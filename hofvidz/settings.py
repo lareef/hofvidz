4172a099d293014be214bfa9163e6ce4a1ff4db6
Lareef <lareef.lafir@gmail.com>
2020-12-18 17:01:49 +0530

    Update settings.py

--- hofvidz/settings.py
+++ hofvidz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3&mridd&qf(sv9_)l3e@nru=p(ebntft^goo!!04h*wyter)m+'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
